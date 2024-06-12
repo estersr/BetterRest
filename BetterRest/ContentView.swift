975d3f67b0ecd41dc1461482ca967e863871d884
estersr <estherramoos@gmail.com>
2024-06-12 17:14:47 -0300

    dateComponents

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,10 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var sleepAmount = 8.0
-    
     var body: some View {
-        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+        //f we just wanted the time from a date we would write this:
+        Text(Date.now, format: .dateTime.hour().minute())
+            .padding()
+        //if we wanted the day, month, and year, we would write this:
+        Text(Date.now, format: .dateTime.day().month().year())
+            .padding()
+        // the formatted() method directly on dates, passing in configuration options for how we want both the date and the time to be formatted
+        Text(Date.now.formatted(date: .long, time: .shortened))
+    }
+    
+    func exampleDates() {
+        //        var components = DateComponents()
+        //        components.hour = 8
+        //        components.minute = 0
+        //        let date = Calendar.current.date(from: components) ?? .now
+        //    }
+        let components = Calendar.current.dateComponents([.hour, .minute], from: .now)
+        let hour = components.hour ?? 0
+        let minute = components.minute ?? 00
     }
 }
 
